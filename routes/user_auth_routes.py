e24a6446dd18bc484516593bd45cfd58d06a1b31
arafatsyed <55632837+arafatsyed@users.noreply.github.com>
2023-10-19 16:31:42 -0400

    add skeleton code for user auth and rename api route function names (#29)

--- routes/user_auth_routes.py
+++ routes/user_auth_routes.py
@@ -2,12 +2,12 @@ from flask import Blueprint
 
 user_auth = Blueprint('user_auth', __name__)
 
-@user_auth.route('/register')
-def create_explore_feed():
+@user_auth.route('/register', method = ['POST'])
+def register_user():
     #TODO
     return "register account"
 
 @user_auth.route('/login')
-def create_following_feed():
+def login():
     #TODO
     return "login"
