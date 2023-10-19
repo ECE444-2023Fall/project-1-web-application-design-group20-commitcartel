b5408e7ae7cc373bba330923703395f9dba0337b
arafatsyed <Arafat.syedshah@mail.utoronto.ca>
2023-10-19 15:16:50 -0400

    add skeleton code for user auth and rename api route function names

--- routes/event_feed_routes.py
+++ routes/event_feed_routes.py
@@ -2,17 +2,17 @@ from flask import Blueprint
 
 event_feed = Blueprint('event_feed', __name__)
 
-@event_feed.route('/explore_feed')
-def create_explore_feed():
+@event_feed.route('/explore_feed', methods = ['GET'])
+def get_explore_feed():
     #TODO
     return "a json object of current events to explore"
 
-@event_feed.route('/following_feed')
-def create_following_feed():
+@event_feed.route('/following_feed', methods = ['GET'])
+def get_following_feed():
     #TODO
     return "a json object of current events from users following"
 
-@event_feed.route('/favourite_event')
-def create_following_feed():
+@event_feed.route('/favourite_event', methods = ['POST'])
+def update_favourite_event():
     #TODO
     return "append the event to the favourite list of user"
