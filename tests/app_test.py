eedd911c4a41ab240f7af73aefbe086f80b4800b
adrianchow-tech <81934116+adrianchow-tech@users.noreply.github.com>
2023-10-28 22:43:18 -0400

    Include user search unit test

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,3 +1,16 @@
 import pytest
 
 @pytest.fixture
+def test_search_events_with_user_input(client, user_search_string):
+    response = client.post('/search', data={'query': user_search_string})
+
+    # Check if the response contains the search string
+    assert user_search_string.encode() in response.data
+
+    # Parse the response as JSON
+    response_data = json.loads(response.data)
+
+    # Check if the response is a list of events that contain the user's input string
+    # This assumes that the database will use "name" as a var that holds each Event's name
+    for event in response_data:
+        assert user_search_string in event['name']
