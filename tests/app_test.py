634eac30eb8cebbec5cb36e2d61cfad09cf3c189
Ismail Bennani <48138437+Ismail-Ben@users.noreply.github.com>
2023-10-29 19:00:57 -0400

    Added test for creating posts

--- tests/app_test.py
+++ tests/app_test.py
@@ -10,6 +10,21 @@ def client():
     with app.app_context():
         yield app.test_client() # tests run here
 
+def test_make_post(client):
+    # Create a sample post data as a dictionary
+    post_data = {"title": "Test Post", "content": "This is a test post."}
+
+    # Send a POST request to the /make_post route with the sample post data
+    response = client.post('/make_post', json=post_data)
+
+    # Check if the response status code is 200 (OK)
+    assert response.status_code == 200
+
+    # Parse the response as JSON
+    response_data = json.loads(response.data)
+
+    # Check if the response contains the expected message
+    assert response_data["message"] == "Post created successfully"
 
 def test_search_events_with_user_input(client, user_search_string):
     response = client.post('/search', data={'query': user_search_string})
