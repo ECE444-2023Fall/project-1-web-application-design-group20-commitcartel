32fd806a258744ce971e5725e2cd982c4276af7b
Ismail Bennani <48138437+Ismail-Ben@users.noreply.github.com>
2023-10-29 19:09:10 -0400

    Moved new make_post test down

--- tests/app_test.py
+++ tests/app_test.py
@@ -10,21 +10,6 @@ def client():
     with app.app_context():
         yield app.test_client() # tests run here
 
-def test_make_post(client):
-    # Create a sample post data as a dictionary
-    post_data = {"title": "Test Post", "content": "This is a test post."}
-
-    # Send a POST request to the /make_post route with the sample post data
-    response = client.post('/make_post', json=post_data)
-
-    # Check if the response status code is 200 (OK)
-    assert response.status_code == 200
-
-    # Parse the response as JSON
-    response_data = json.loads(response.data)
-
-    # Check if the response contains the expected message
-    assert response_data["message"] == "Post created successfully"
 
 def test_search_events_with_user_input(client, user_search_string):
     response = client.post('/search', data={'query': user_search_string})
@@ -45,3 +30,19 @@ def test_delete_event(client, event_id):
     # Ensure the event is being deleted
     rv = client.delete("/event_post/1")
     assert rv.status_code == 200
+
+def test_make_post(client):
+    # Create a sample post data as a dictionary
+    post_data = {"title": "Test Post", "content": "This is a test post."}
+
+    # Send a POST request to the /make_post route with the sample post data
+    response = client.post('/make_post', json=post_data)
+
+    # Check if the response status code is 200 (OK)
+    assert response.status_code == 200
+
+    # Parse the response as JSON
+    response_data = json.loads(response.data)
+
+    # Check if the response contains the expected message
+    assert response_data["message"] == "Post created successfully"
