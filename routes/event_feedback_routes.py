51639ca762e93b34b51d47e4388277b14f3ed9e3
KavyaKadi3 <kavyakadi@gmail.com>
2023-11-07 17:33:02 -0500

    Update event_feedback_routes.py with removed print debug statements

--- routes/event_feedback_routes.py
+++ routes/event_feedback_routes.py
@@ -12,15 +12,12 @@ def leave_event_feedback():
     user_id = data['user_id']
     rating = data['rating']
     comments = data['comments']
-    print(feedback_id, event_id, user_id, rating, comments)
 
     # Add the event rating and comments to corresponding event                        
     success_events, result_events = update_one('Events', {'_id': ObjectId(event_id)}, {'$addToSet': {'event_rating': rating, 'event_comments': comments}})
-    print(success_events, result_events)
 
     # Add the event rating and comments to Ratings
     success_ratings, result_ratings = update_one('Ratings', {'_id': ObjectId(feedback_id)}, {'$set': {'event_id': ObjectId(event_id), 'user_id': ObjectId(user_id), 'rating': rating, 'comments': comments}})
-    print(success_ratings, result_ratings)
 
     if success_events and success_ratings:
         return jsonify({'message': result_events + " " + result_ratings})
