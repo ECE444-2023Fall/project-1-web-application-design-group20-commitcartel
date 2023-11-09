c2decc93890e2f6ca5755299042a77aa403f95d6
KavyaKadi3 <kavyakadi@gmail.com>
2023-11-09 17:21:42 -0500

    Update event_feedback_routes.py with latest Events database structure

--- routes/event_feedback_routes.py
+++ routes/event_feedback_routes.py
@@ -7,14 +7,18 @@ event_feedback = Blueprint('event_feedback', __name__)
 @event_feedback.route('/leave_event_feedback', methods=['POST'])
 def leave_event_feedback():
     data = request.json  # TODO: Adapt implementation depending on frontend
-    feedback_id = data['_id']
     event_id = data['event_id']
-    user_id = data['user_id']
-    rating = data['rating']
-    comments = data['comments']
+    feedback_id = ObjectId()
+    
+    event_object = {
+        'event_feedback_id': str(feedback_id),
+        'user_id': data['user_id'],
+        'rating': data['rating'],
+        'comments': data['comments'],
+    }
 
     # Add the event rating and comments to corresponding event                        
-    success_events, result_events = update_one('Events', {'_id': ObjectId(event_id)}, {'$addToSet': {'feedback_id': ObjectId(feedback_id), 'user_id': ObjectId(user_id), 'event_rating': rating, 'event_comments': comments}})
+    success_events, result_events = update_one('Events', {'_id': ObjectId(event_id)}, {'$addToSet': {'event_ratings': event_object}})
 
     if success_events:
         return jsonify({'message': result_events})
