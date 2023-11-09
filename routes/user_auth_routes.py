9a2dfad9a9b5d4954a4ac0d8a770954e5ab8ccb0
KavyaKadi3 <kavyakadi@gmail.com>
2023-11-09 15:15:38 -0500

    Adding functionality for Create User Account (#59)
    * Adding functionality for Create User Account
    
    * Updating /create_user_account route to match with intended UI design
    
    * Updating question fields to include program and year and adding redirect to login page

--- routes/user_auth_routes.py
+++ routes/user_auth_routes.py
@@ -2,11 +2,6 @@ from flask import Blueprint
 
 user_auth = Blueprint('user_auth', __name__)
 
-@user_auth.route('/register', method = ['POST'])
-def register_user():
-    #TODO
-    return "register account"
-
 @user_auth.route('/login')
 def login():
     #TODO
