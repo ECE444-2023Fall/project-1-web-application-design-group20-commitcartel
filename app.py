1728cf26103da2cf301c29d036d2e2b9a0ea1e00
arafatsyed <Arafat.syedshah@mail.utoronto.ca>
2023-11-09 19:27:34 -0500

    remove commented functions from app.py

--- app.py
+++ app.py
@@ -41,12 +41,6 @@ class NameForm(FlaskForm):
     email       = StringField( 'Enter your Email Address', validators = [DataRequired(), validateEmail()] )
     submit      = SubmitField( 'Submit' )
 
-# @app.route('/clubs/<club_id>/<event_id>', method=["GET"])
-# def clubs(club_id, event_id):
-#     # determine if event has happened
-
-#     return  render_template('clubs.html')
-
 @app.route('/')
 def index():
     return render_template("homepage.html")
