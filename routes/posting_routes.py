63cd1c3fa3e3363b4b6a251eeb30ae130d6221fe
arafatsyed <Arafat.syedshah@mail.utoronto.ca>
2023-11-11 20:01:20 -0500

    fix club_id index

--- routes/posting_routes.py
+++ routes/posting_routes.py
@@ -90,11 +90,11 @@ def create_event():
             return redirect(url_for('posting.create_event'))
         
         # append events array for club
-        success, count = update_one('Clubs', {'_id': ObjectId("654456e2dfd8c6673e217e8f")}, {'$addToSet': {'events': ObjectId(id)}}) # club_id should be session['club_id']
+        success, count = update_one('Clubs', {'_id': ObjectId(club_id)}, {'$addToSet': {'events': ObjectId(id)}}) # club_id should be session['club_id']
         if not success:
             return redirect(url_for('posting.create_event'))
     
-        return redirect(url_for('club_pg.club_event_view', club_id="654456e2dfd8c6673e217e8f", event_id=str(id))) # club_id should be session['club_id']
+        return redirect(url_for('club_pg.club_event_view', club_id=str(club_id), event_id=str(id))) # club_id should be session['club_id']
     return render_template('create_event.html', form=form)
 
 # Route to create a new post (POST request)
