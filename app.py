8ed4660772a7f07d0a3f472a7dabb6d465b84011
Navidur Rahman <navid2001@gmail.com>
2023-11-12 00:22:33 -0500

    Fix small errors

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def index():
 
 @app.before_request
 def check_session():
-    if (request.endpoint is not 'static'):
+    if (request.endpoint != 'static'):
         # Get the user type from the session
         user_type = 'anonymous'
         if session.get('is_user') is not None:
@@ -99,8 +99,10 @@ def check_session():
         if request.endpoint not in ALLOWED_ROUTES[user_type]:
             if user_type == 'anonymous':
                 return redirect(url_for('user_auth.login'))
+            
             elif user_type == 'club':
                 return redirect(url_for('club_pg.clubs', club_id = session['club_id']))
+            
             elif user_type == 'user':
                 return redirect(url_for('index'))
 
