ebfa62ac47ce29d3f6fdb00640dbe0af9665dd81
Navidur Rahman <navid2001@gmail.com>
2023-11-14 01:01:59 -0500

    Add sleeps before calls

--- tests/app_test.py
+++ tests/app_test.py
@@ -3,6 +3,7 @@ import json
 from pathlib import Path
 from app import app
 from database import db_client, insert_one, update_one, get_data_one, get_data, delete_one
+import time
 
 @pytest.fixture
 def client():
@@ -13,11 +14,10 @@ def client():
 @pytest.fixture
 def db():
     # Empty out the DB
-    db_client['Test'].delete_many()
+    db_client['Test'].delete_many(filter={})
     yield db
 
-
-def test_insert_one():
+def test_insert_one(db):
     # Sample data
     data = {'Name': 'Test', 'program': 'Test Program'}
 
@@ -30,22 +30,24 @@ def test_insert_one():
 def test_update_one():
     # Sample data
     insert_data = {'Name': 'Test2', 'program': 'Test Program 2'}
-    insert_one('Test', insert_data)
+    _, id = insert_one('Test', insert_data)
 
     data = {'$set': {'program': 'New Program'}}
 
     # Update the previously created user
-    response = update_one('Test', {'Name': 'Test2'}, data)
+    time.sleep(.5)
+    response = update_one('Test', {'_id': id}, data)
 
     # Check if the response is correct
     assert response == (True, "Update Successful")
 
 def test_get_data_one():
     insert_data = {'Name': 'Test3', 'program': 'Test Program 3'}
-    insert_one('Test', insert_data)
+    _, id = insert_one('Test', insert_data)
 
     # Get the previously created user
-    response = get_data_one('Test', {'Name': 'Test3'})
+    time.sleep(.5)
+    response = get_data_one('Test', {'_id': id})
 
     # Remove the object ID from response
     del response[1]['_id']
@@ -55,10 +57,11 @@ def test_get_data_one():
 
 def test_delete_one():
     insert_data = {'Name': 'Test4', 'program': 'Test Program 4'}
-    insert_one('Test', insert_data)
+    _, id = insert_one('Test', insert_data)
 
     # Delete the previously created user
-    response = delete_one('Test', {'Name': 'Test4'})
+    time.sleep(.5)
+    response = delete_one('Test', {'_id': id})
 
     # Check if the response is correct
     assert response == (True, "Delete Successful")
